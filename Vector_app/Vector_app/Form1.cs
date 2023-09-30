1aab3aa2d7400c18fa358285c09c13a2ec700468
Vitaliy <ivi.22@uni-dubna.ru>
2023-10-01 01:36:33 +0300

    new file:   Vector_app/Vector_app.sln 	new file:   Vector_app/Vector_app/App.config 	new file:   Vector_app/Vector_app/Form1.Designer.cs 	new file:   Vector_app/Vector_app/Form1.cs 	new file:   Vector_app/Vector_app/Form1.resx 	new file:   Vector_app/Vector_app/MyVectEnum.cs 	new file:   Vector_app/Vector_app/MyVector.cs 	new file:   Vector_app/Vector_app/Program.cs 	new file:   Vector_app/Vector_app/Properties/AssemblyInfo.cs 	new file:   Vector_app/Vector_app/Properties/Resources.Designer.cs 	new file:   Vector_app/Vector_app/Properties/Resources.resx 	new file:   Vector_app/Vector_app/Properties/Settings.Designer.cs 	new file:   Vector_app/Vector_app/Properties/Settings.settings 	new file:   Vector_app/Vector_app/Vector_app.csproj

---
+++ Vector_app/Vector_app/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Vector_app
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        private MyVector<int> myvector = new MyVector<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_Refresh()
+        {
+            listBox1.Items.Clear();
+            foreach (var item in myvector)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void CapSize_Refresh()
+        {
+            tb_capacity.Text = myvector.Capacity.ToString();
+            tb_size.Text = myvector.Size.ToString();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myvector.Add(Convert.ToInt32(tb_add.Text));
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введите число");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+
+            CapSize_Refresh();
+            listBox1_Refresh();
+        }
+
+        private void btn_setvalue_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tb_setvalue.Text = myvector.SetValue(Convert.ToInt32(tb_setvalue.Text)).ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введите число");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+            CapSize_Refresh();
+        }
+
+        private void btn_resize_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myvector.Resize(Convert.ToInt32(tb_resize.Text));
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введите число");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+
+            CapSize_Refresh();
+            listBox1_Refresh();
+        }
+
+        private void btn_reserve_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myvector.Reserve(Convert.ToInt32(tb_reserve.Text));
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введите число");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+
+            CapSize_Refresh();
+            listBox1_Refresh();
+        }
+
+        private void btn_shrinkto_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myvector.ShrinkToFit();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введите число");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+
+            CapSize_Refresh();
+        }
+
+        private void btn_random_Click(object sender, EventArgs e)
+        {
+            myvector = new MyVector<int>((int)numericUpDown1.Value);
+            for (int i = 0; i < myvector.Capacity; i++)
+            {
+                int item = random.Next(100);
+                myvector.Add(item);
+            }
+            listBox1_Refresh();
+            CapSize_Refresh();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            myvector = new MyVector<int>((int)numericUpDown1.Value);
+            CapSize_Refresh();
+            listBox1_Refresh();
+        }
+    }
+}
---
+++ Vector_app/Vector_app/MyVector.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_app
+{ 
+    public class MyVector<T> : IEnumerable<T>
+    {
+        T[] _data;
+        int size;
+        public MyVector()
+        {
+            Capacity = 1;
+            Size = 0;
+        }
+        public MyVector(int max_size)
+        {
+            Capacity = max_size;
+            Size = 0;
+        }
+
+        public int Size
+        {
+            get { return size; }
+            private set { size = value; }
+        }
+        public int Capacity
+        {
+            get { return _data.Length; }
+            private set { Array.Resize(ref _data, value); }
+        }
+
+        public void Add(T item)
+        {
+            if (Size == Capacity) Capacity *= 2;
+                
+            _data[Size] = item;
+            Size++;
+           
+        }
+        
+        public T SetValue(int item)
+        {
+            if(item < 0 ||  item > Size) throw new Exception("Индекс вне массива");
+            return _data[item];
+        }
+
+        public void Resize(int newSize)
+        {
+            if (newSize <= 0) throw new Exception("Неверное число");
+
+            if (newSize < Size) Size = newSize;
+            Capacity = newSize;
+        }
+
+        public void Reserve(int newCapacity)
+        {
+            if (newCapacity <= 0) throw new Exception("Неверное число");
+
+            if (newCapacity < Size)
+            {
+                    Size = newCapacity;
+                    Capacity = Size;
+            }
+            else Capacity = newCapacity;
+        }
+
+        public void ShrinkToFit()
+        {
+            Capacity = Size;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new MyVectEnum<T>(_data, Size);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+}
+
